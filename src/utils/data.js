30a2cb1eb6d7d8c3ce289184b7ad802c7049f5cf
oren0115 <nyomana369@gmail.com>
2024-12-04 11:05:52 +0700

    Initial commit

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: "Diams Saputra",
+      tag: "dimasmds",
+      imageUrl: "./public/images/dimasmds.jpeg",
+    },
+    {
+      id: 2,
+      name: "Arif Faizin",
+      tag: "ariffaizin",
+      imageUrl: "./public/images/arifaizin.jpeg",
+    },
+    {
+      id: 3,
+      name: "Rahmat Fajri",
+      tag: "rahmatfajri",
+      imageUrl: "./public/images/rfajri27.jpeg",
+    },
+  ];
+};
+
+export { getData };
